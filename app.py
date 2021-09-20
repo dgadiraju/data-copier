df54e7a14fc04c8649d81c379787665e999c219f
Durga Gadiraju <dgadiraju@itversity.com>
2021-09-20 09:55:24 +0000

    Read and write logic added to data copier

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
+import os
+import sys
+from read import get_json_reader
+from write import load_db_table
+
+
+def process_table(BASE_DIR, conn, table_name):
+    json_reader = get_json_reader(BASE_DIR, table_name)
+    for df in json_reader:
+        load_db_table(df, conn, table_name, df.columns[0])
+
+
 def main():
-    print('Hello World from itversity')
+    BASE_DIR = os.environ.get('BASE_DIR')
+    table_names = sys.argv[1].split(',')
+    configs = dict(os.environ.items())
+    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+    for table_name in table_names:
+        process_table(BASE_DIR, conn, table_name)
 
 
 if __name__ == "__main__":
