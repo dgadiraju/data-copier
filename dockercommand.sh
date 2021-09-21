b50740391b5ba7ef8e2990820fe2a0502a3da924
Durga Gadiraju <dgadiraju@itversity.com>
2021-09-21 14:26:26 +0000

    Dockerized the application.

---
+++ dockercommand.sh
@@ -0,0 +1,15 @@
+docker run -it --name data-copier --rm --network data-copier-nw -v C:\Users\dgadiraju\Research\data\retail_db_json:/retail_db_json -e BASE_DIR=/retail_db_json -e DB_HOST=38db1223009f -e DB_PORT=5432 -e DB_NAME=retail_db -e DB_USER=retail_user -e DB_PASS=itversity data-copier python /data-copier/app/app.py departments,categories
+
+docker run \
+  -it \
+  --name data-copier \
+  --rm \
+  --network data-copier-nw \
+  -v /Users/itversity/Research/data/retail_db_json \
+  -e BASE_DIR=/retail_db_json \
+  -e DB_HOST=38db1223009f \
+  -e DB_PORT=5432 \
+  -e DB_NAME=retail_db \
+  -e DB_USER=retail_user \
+  -e DB_PASS=itversity \
+  data-copier bash
